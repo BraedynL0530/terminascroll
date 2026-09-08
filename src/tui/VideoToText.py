5af913fdea678431cc0efd1e270de59703f27ceb
braedynl <braedynlea@gmail.com>
2026-09-07 20:46:57 -0400

    Add frame to char + rgb compatible

--- src/tui/VideoToText.py
+++ src/tui/VideoToText.py
@@ -1,5 +1,6 @@
 from typing import Optional
 import cv2
+import os
 
 #ive been told in reviews that static typing is useful and makes code more readable,
 #if anyone sees this file tell me if i did too much!
@@ -20,11 +21,45 @@ def downloader(video_url: str) -> None:
         pass#delete the mp4 files(3 stays for sound)
     pass
 
-def mp4_to_text(mp4_path: str) -> str:
+def mp4_to_text(mp4_path: str, width: int = 120) -> None:
     cap = cv2.VideoCapture(mp4_path)
-    while cap.isOpened():
-        success, frame = cap.read()
-        if not success:
-            break
-        #yip yap
-    pass
+
+    ascii_chars = "@#$%m*+=+-:.·` "
+    num_chars = len(ascii_chars)
+
+    os.system("")
+
+    try:
+        while cap.isOpened():
+            success, frame = cap.read()
+            if not success:
+                break
+
+            o_height, o_width = frame.shape[:2] #orgial
+
+            height = int((o_height / o_width) * width * 0.55)
+            resized = cv2.resize(frame, (width, height))#resizing for ratio
+
+            frame = []
+
+            for row in resized:
+                line = ""
+                for pixel in row:
+                    b, g, r = pixel
+
+                    """had to look this up because i overcomplicated stuff and didnt do grayscale
+                    (299 * r + 0.587 * g + 0.114 * b)
+                    apparently this is formula for rgb brightness? or human eye brightness? dunno how it works.
+                    """
+
+                    brightnes = (299 * r + 0.587 * g + 0.114 * b)
+                    char_idx = (brightnes * (num_chars - 1)) / 255# ill miss spell all i want pycharm :P
+                    char = ascii_chars[char_idx]
+
+                    line += f"\033[38;2;{r};{g};{b}m{char}"
+
+                frame.append(line + "\033[0m")
+
+
+    except:
+        pass
